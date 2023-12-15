6155ce6c9442090efccfa694823745a8d5322235
Joelson Leal <joelsonleal@outlook.com>
2023-12-15 16:13:07 -0100

    Revert "font styles and link target update"
    This reverts commit af8862919d0405bb888c032472f22421b7baef52.

--- pages/about.js
+++ pages/about.js
@@ -38,7 +38,7 @@ export default function Home({ designers }) {
 
       <Title
         className="title m0 p0"
-        text="kriolus*who&nbsp;design&nbsp;is&nbsp;a"
+        text="Kriolus*who&nbsp;design&nbsp;is&nbsp;a"
         noAnimation
       />
 
--- pages/nominate.js
+++ pages/nominate.js
@@ -7,12 +7,26 @@ export default function Nominate({ designers }) {
   return (
     <div className="container">
       <Head>
-        <title>Kriolus Who Design | Nominate</title>
+        <title>Brazilians Who Design | Nomiate</title>
         <link rel="icon" href="/favicon.png" />
         <MetaTags />
       </Head>
 
-      <div data-tf-live="01HHPM2BST8FF8NZ3CZSC1VBPC"></div><script src="//embed.typeform.com/next/embed.js"></script>
+      <div style={{ width: "100%", position: "relative", height: "100vh" }}>
+        <iframe
+          id="typeform-full"
+          width="100%"
+          height="100%"
+          frameBorder="0"
+          allow="camera; microphone; autoplay; encrypted-media;"
+          src="https://form.typeform.com/to/bbrPUqMe"
+        ></iframe>{" "}
+        <script
+          type="text/javascript"
+          src="https://embed.typeform.com/embed.js"
+        ></script>
+        <Analytics />
+      </div>
 
       <Link href="/" shallow={true}>
         <a className="auxNav arrowback">←</a>
