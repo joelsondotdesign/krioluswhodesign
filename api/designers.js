27350c522c1239d483a8e0a978a739db3d3dccc9
Joelson Leal <joelsonleal@outlook.com>
2024-02-11 16:06:27 -0100

    try to solve not loading KWD data

--- api/designers.js
+++ api/designers.js
@@ -5,7 +5,7 @@ export default async (req, res) => {
     const auth = new google.auth.GoogleAuth({
       credentials: {
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY,
+        private_key: JSON.parse(process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')),
       },
       scopes: [
         "https://www.googleapis.com/auth/drive",
