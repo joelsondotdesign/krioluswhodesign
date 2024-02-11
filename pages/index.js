91bfd87687662ffe2da7f9aee001f604d66982f5
Joelson Leal <joelsonleal@outlook.com>
2024-02-11 14:28:36 -0100

    new commit desperate

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export async function getStaticProps() {
   const origin =
     process.env.NODE_ENV !== "production"
       ? "http://localhost:3000"
-      : "https://krioluswho.design/";
+      : "https://brazilianswho.design/";
 
   console.log(origin)
 
