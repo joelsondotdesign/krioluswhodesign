4188fe6d5476db1a1856fb16a2104d8a96187fd1
Fábio Alves <fabioalves.contacto@gmail.com>
2024-04-18 20:14:28 -0100

    Add airtable embed

--- pages/nominate.js
+++ pages/nominate.js
@@ -12,11 +12,19 @@ export default function Nominate({ designers }) {
         <MetaTags />
       </Head>
 
-      <div data-tf-live="01HHPM2BST8FF8NZ3CZSC1VBPC"></div><script src="//embed.typeform.com/next/embed.js"></script>
-
       <Link href="/" shallow={true} legacyBehavior>
         <a className="auxNav arrowback">←</a>
       </Link>
+      <iframe 
+        className="airtable-embed" 
+        src="https://airtable.com/embed/appbCYV4xmMm8P3gP/pago7HkjvD8cNmr7s/form" frameborder="0" onmousewheel="" width="100%" height="640" 
+        style={{
+          background: '#0D56F6',
+          border: '1px solid #ccc',
+          marginTop: '5rem'
+        }}
+      />
+
       <style jsx global>{`
         html,
         body {
@@ -26,14 +34,6 @@ export default function Nominate({ designers }) {
         }
         .container {
           min-height: 100vh;
-        }
-        iframe {
-          position: fixed;
-          left: 0;
-          right: 0;
-          bottom: 0;
-          top: 0;
-          border: 0;
         }
       `}</style>
     </div>
