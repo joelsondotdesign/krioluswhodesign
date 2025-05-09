5b651635e5e1b35598c5e41a4553629895c77ddb
Joelson Leal <joelsonleal@outlook.com>
2025-05-09 14:08:13 +0100

    new nominate form from typeform

--- pages/nominate.js
+++ pages/nominate.js
@@ -12,10 +12,33 @@ export default function Nominate({ designers }) {
         <MetaTags />
       </Head>
 
+      <div style={{ width: "100%", position: "relative", height: "100vh" }}>
+
+        <div data-tf-live="01HHPM2BST8FF8NZ3CZSC1VBPC"></div><script src="//embed.typeform.com/next/embed.js"></script>
+        
+        {/* <iframe
+          id="typeform-full"
+          width="100%"
+          height="100%"
+          border="0"
+          allow="camera; microphone; autoplay; encrypted-media;"
+          src="https://form.typeform.com/to/bbrPUqMe"
+        ></iframe>{" "} */}
+
+        {/* <script
+          type="text/javascript"
+          src="https://embed.typeform.com/embed.js"
+        ></script> */}
+
+        <Analytics />
+
+      </div>
+
       <Link href="/" shallow={true} legacyBehavior>
         <a className="auxNav arrowback">←</a>
       </Link>
-      <iframe 
+     
+     {/* <iframe 
         className="airtable-embed" 
         src="https://airtable.com/embed/appbCYV4xmMm8P3gP/pago7HkjvD8cNmr7s/form" frameborder="0" onmousewheel="" width="100%" height="640" 
         style={{
@@ -23,7 +46,7 @@ export default function Nominate({ designers }) {
           border: '1px solid #ccc',
           marginTop: '5rem'
         }}
-      />
+      /> */}
 
       <style jsx global>{`
         html,
@@ -34,6 +57,14 @@ export default function Nominate({ designers }) {
         }
         .container {
           min-height: 100vh;
+        }
+        iframe {
+          position: fixed;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          top: 0;
+          border: 0;
         }
       `}</style>
     </div>
