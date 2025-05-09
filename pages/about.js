189c33e032b2dd2e978cb76de7ae722e83ae0c48
Joelson Leal <joelsonleal@outlook.com>
2025-05-09 12:03:23 +0100

    added target blank to links

--- pages/about.js
+++ pages/about.js
@@ -190,13 +190,13 @@ export default function Home({ designers }) {
             Thanks to Fábio Alves, Julia Coronel,
             Milton Cabral, Helder de Pina, Kasey Randall,
             for the help putting together this project of gathering the initial list of designers.
-            Also, a special shoutout to <a className="link" ref="https://x.com/zehf">Zeh Fernandes</a> for all the support 
+            Also, a special shoutout to <a className="link" href="https://x.com/zehf" target="_blank">Zeh Fernandes</a> for all the support 
           </p>
 
           <h3>Who’s behind this?</h3>
           <ul>
             <li>
-              <a className="link" href="https://www.linkedin.com/in/joelsonleal/">
+              <a className="link" href="https://www.linkedin.com/in/joelsonleal/"target="_blank">
                 Joelson Leal
               </a>
             </li>
